e50342a62267f80dba552fe4f23e314ce94ef427
svyatoslav-okshin <svyatokshin@gmail.com>
2020-01-27 16:37:46 -0500

    Met MVP and also styled the index.js file so text i centered

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLinks = styled.div`
+  text-align: center;
+`
 
 const Navigation = () => {
   return (
-    <div>
+    <StyledLinks>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to="">Home</Link>
         </div>
         <div>
-          <a href="">About</a>
+         <Link to="./About">About</Link>
         </div>
         <div>
-          <a href="">Contact</a>
+        <Link to="./Contact">Contact</Link>
         </div>
       </div>
-    </div>
+    </StyledLinks>
   );
 };
 
